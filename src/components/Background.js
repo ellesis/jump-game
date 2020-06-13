3c039a4f8ae14ca049677e267b03a4a25b7c53e8
Lisa Han <ellesis@gmail.com>
2020-06-12 21:33:01 -0700

    update <Background> with image and moving

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,12 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
-
 import { makeStyles } from '@material-ui/core/styles';
 import BackgroundImg from '../image/background.png';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-
-  },
+  root: {},
   background: {
     border: '1px solid black',
     margin: '50px 0 0 50px',
@@ -14,11 +11,63 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
+
 function Background() {
+  // init
+  const updateTime = 20 // setInterval updateTime
+  const speed = 5   // move speed
+  const interval = useRef()
+
+  // State
+  const [offSet, setOffSet] = useState(0)
+
+
+  // useEffect
+  useEffect(() => {
+    return () => {
+      clearInterval(interval.current)
+    }
+  }, []) // componentDidMount
+  useEffect(() => {
+    draw()
+    setBackgroundMovement()
+    return () => {
+      clearInterval(interval.current)
+    }
+  }, [offSet])  // depend on offSet change
+
+  // background move
+  const setBackgroundMovement = () => {
+    interval.current = setInterval(() => {
+      if (offSet == 1000-speed) {
+        setOffSet(0)
+      } else {
+        setOffSet(offSet + speed)
+      }
+    }, updateTime)
+  }
+
+  // background image dwaw
+  const draw = () => {
+    let canvas = document.querySelector('canvas#background') //background canvas
+    let ctx = canvas.getContext('2d')
+    let imageObj = new Image()
+    imageObj.onload = function() {
+      // context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
+      // left
+      ctx.drawImage(imageObj, offSet, 0, 1000-offSet, 500, 0, 0, 1000-offSet, 500)
+      // right
+      ctx.drawImage(imageObj, 0, 0, offSet, 500, 1000-offSet, 0, offSet, 500)
+    }
+    
+    imageObj.src = BackgroundImg  //Add image
+  }
+
   const classes = useStyles();  // css
+  // Canvas
   return (
     <div>
-      <canvas width="1000px" height="500px" id="background" className={classes.background}>
+      <canvas id="background" width="1000px" height="500px" className={classes.background}>
       </canvas>
     </div>
   );

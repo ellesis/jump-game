76b8cfbdc158ee7caf7f59b9fedfea8cb570bc51
Lisa Han <ellesis@gmail.com>
2020-06-12 21:48:38 -0700

    update App main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React,{ useState, useEffect, useRef } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
 import Background from './components/Background';
 
-import './App.css';
+const useStyles = makeStyles(theme => ({
+  root: {},
+}))
 
 const App = () => {
+  // state
+  // const [isStart, setIsStart] = useState(false);
+
   return (
     <div>
-      <Background />
+      {
+        // isStart ?
+          <div>
+            <Background />
+          </div>
+        // :
+        //   <div>
 
+        //   </div>
+      }
     </div>
   );
 }

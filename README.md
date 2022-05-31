2d311e3d103395b55b77258821c2ac89815a5da8
Lisa Han <ellesis@gmail.com>
2022-05-31 09:18:35 -0700

    fix collision function

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Jump Game
+
+- React
+  - npm start
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React,{ useState, useEffect, useRef } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core/';
+import React, {useState, useEffect, useRef} from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import {Button} from '@material-ui/core/'
 
+import Background from './components/Background'
+import Character from './components/Character'
+import Enemy from './components/Enemy'
 
-import Background from './components/Background';
-import Character from './components/Character';
-import Enemy from './components/Enemy';
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   button: {
     border: '1px solid black',
     margin: '50px 0 0 50px',
@@ -23,7 +22,7 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     left: '50px',
     top: '570px',
-    fontSize: "20px"
+    fontSize: '20px'
   }
 }))
 
@@ -36,20 +35,20 @@ const App = () => {
   // useState
   const [isStart, setIsStart] = useState(false) // start or not
   const [time, setTime] = useState(0)
-  const [result, setResult] = useState(0)   // Score
+  const [result, setResult] = useState(0) // Score
   const [isMove, setIsMove] = useState(false)
   // useEffect - update
   useEffect(() => {
     if (isStart) {
       interval.current = setInterval(() => {
-        // 2초마다 한번씩 isMove 값 change
-        if (Math.floor(time)%2 === 1) {
+        // by 2 second isMove value change
+        if (Math.floor(time) % 2 === 1) {
           setIsMove(true)
         } else {
           setIsMove(false)
         }
         setTime(time + updateTime * 0.001)
-        checkConclict()
+        checkConflict()
       }, updateTime)
     }
 
@@ -58,16 +57,18 @@ const App = () => {
     }
   }, [time, isStart])
 
-  const checkConclict = () => { // collision
+  const checkConflict = () => {
+    // collision
     let enemy = document.querySelector('img#enemy')
     let character = document.querySelector('img#character')
 
     if (enemy !== null && character !== null) {
-      // console.log(enemy.x)
-      // console.log(character.x)
+      // console.log(`>>>: checkConflict -> character.x`, character.x)
+      // console.log(`>>>: checkConflict -> enemy.x`, enemy.x)
       let dis = Math.pow(enemy.x - character.x, 2) + Math.pow(enemy.y - character.y, 2)
-      if (dis < 3000) {
-        alert("Game Over!")
+      // console.log(`>>>: checkConflict -> dis`, dis)
+      if (dis < 28000) {
+        alert('Game Over!')
         if (result < time) setResult(time) // update high score
         setIsStart(false)
         setTime(0)
@@ -81,28 +82,28 @@ const App = () => {
 
   return (
     <div>
-      {
-        isStart ?
-          <div>
-            <Background />
-            <Character />
-            <Enemy isMove={isMove}/>
-            <div className={classes.timer}>
-              <div>Jump Game!</div>
-              <div style={{margin:"0 0 0 50px"}}>Time:</div>
-              <div style={{margin:"0 0 0 10px"}}>{Math.floor(time)} sconds</div>
-            </div>
+      {isStart ? (
+        <div>
+          <Background />
+          <Character />
+          <Enemy isMove={isMove} />
+          <div className={classes.timer}>
+            <div>Jump Game!</div>
+            <div style={{margin: '0 0 0 50px'}}>Time:</div>
+            <div style={{margin: '0 0 0 10px'}}>{Math.floor(time)} sconds</div>
           </div>
-        :
-          <div>
-            <Button onClick={handleClickStartButton} className={classes.button}>
-              Your Highest Record : {Math.floor(result)} seconds<br/>
-              Click to Start Game!
-            </Button>
-          </div>
-      }
+        </div>
+      ) : (
+        <div>
+          <Button onClick={handleClickStartButton} className={classes.button}>
+            Your Highest Record : {Math.floor(result)} seconds
+            <br />
+            Click to Start Game!
+          </Button>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
